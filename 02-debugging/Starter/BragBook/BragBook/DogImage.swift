76a3c9e7a6c2233e4e964d64b09aefebb10ce561
Walter Tyree <walter@tyreeapps.com>
2024-03-14 10:01:09 -0400

    removes warning in starter

--- 02-debugging/Starter/BragBook/BragBook/DogImage.swift
+++ 02-debugging/Starter/BragBook/BragBook/DogImage.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2023 Kodeco Inc.
-///
+/// Copyright (c) 2024 Kodeco Inc.
+/// 
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-///
+/// 
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-///
+/// 
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-///
+/// 
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -32,50 +32,16 @@
 
 import SwiftUI
 
-struct ContentView: View {
-  @State var dogImages = [
-    "corgi",
-    "costa-rica",
-    "datsuk",
-    "jack-russel",
-    "mattie",
-    "ruth"
-  ]
-  @State var currentImage = 0  // the index of the current image
-
-  let totalImages = 6 // the number of images
-
+struct DogImage: View {
+  let imageToShow: String
   var body: some View {
-    VStack {
-      Text("Dogs")
-        .font(.largeTitle)
-      HStack {
-        Button("", systemImage: "chevron.left") {
-          var backwardsIndex = currentImage - 1
-          if backwardsIndex < 0 {
-            backwardsIndex = totalImages
-          }
-          currentImage = backwardsIndex
-        }
-        Spacer()
-        Button("", systemImage: "chevron.right") {
-          var forwardIndex = currentImage + 1
-          if forwardIndex > totalImages {
-            forwardIndex = 0
-          }
-          currentImage = forwardIndex
-        }
-      }
-      Image(dogImages[currentImage])
-        .resizable()
-        .aspectRatio(contentMode: .fit)
+    Image(imageToShow)
+      .resizable()
+      .aspectRatio(contentMode: .fit)
       .padding([.top, .leading, .trailing])
-    }
   }
 }
 
-struct ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+#Preview {
+  DogImage(imageToShow: "ruth")
 }
